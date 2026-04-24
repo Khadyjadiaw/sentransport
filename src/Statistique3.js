0bc9a8a1c33fdcef3b0848163b66d9df500d06ac
Khadyja <khadydiaw1@esp.sn>
2026-04-24 10:19:36 +0000

    Ajout Footer, date Header et composant Statistique

---
+++ src/Statistique3.js
@@ -0,0 +1,12 @@
+import './Statistique.css';
+
+function Statistique3() {
+  return (
+    <div className="statistique">
+      <span className="stat-chiffre">5000</span>
+      <span className="stat-libelle">voyageurs/jour</span>
+    </div>
+  );
+}
+
+export default Statistique3;
